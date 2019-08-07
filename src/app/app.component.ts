345b09dbb8aa4bdd8fb44d8c6302fe834e3b852a
Partical777 <z25993021@gmail.com>
2019-08-07 15:25:35 +0800

    New Function : Add Playlist Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+
+  constructor(public sanitizer: DomSanitizer){}
+
+  index = 0;
+  
+  loop = true;
+
+  shuffle = false;
+
+  Playlist = 'https://www.youtube.com/embed/videoseries?list=PLxySa8Rx-er_crWm9uz9ETLVrPMpFBuX8';
+
+  PlaylistURL = "https://www.youtube.com/embed/videoseries?list=PLxySa8Rx-er_crWm9uz9ETLVrPMpFBuX8&index=77&autoplay=0&loop=1&cc_load_policy=0&controls=0&iv_load_policy=3&modestbranding=1&showinfo=0&autohide=1";
+
+  GenerateURL(){
+    this.PlaylistURL = this.Playlist + "&autoplay=&cc_load_policy=0&controls=0&iv_load_policy=3&modestbranding=1&showinfo=0&autohide=1";
+    
+    this.PlaylistURL += this.loop ? "&loop=1" : "&loop=0";
+    this.PlaylistURL += "&index=" + this.index;
+  }
+  
 }
