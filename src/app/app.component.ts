8f2591c27ba6028b5bc02c12f765f93686db2faf
Partical777 <z25993021@gmail.com>
2019-08-07 16:40:11 +0800

    New Function : Input Playlist just by link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,15 +16,18 @@ export class AppComponent  {
 
   shuffle = false;
 
-  Playlist = 'https://www.youtube.com/embed/videoseries?list=PLxySa8Rx-er_crWm9uz9ETLVrPMpFBuX8';
+  Playlist = 'https://www.youtube.com/playlist?list=PLxySa8Rx-er9SUSOEbDh1KwguS2y9BL_D';
 
   PlaylistURL = "https://www.youtube.com/embed/videoseries?list=PLxySa8Rx-er_crWm9uz9ETLVrPMpFBuX8&index=77&autoplay=0&loop=1&cc_load_policy=0&controls=0&iv_load_policy=3&modestbranding=1&showinfo=0&autohide=1";
 
   GenerateURL(){
-    this.PlaylistURL = this.Playlist + "&autoplay=&cc_load_policy=0&controls=0&iv_load_policy=3&modestbranding=1&showinfo=0&autohide=1";
+    let res = this.Playlist.slice(38);
+    this.PlaylistURL = "https://www.youtube.com/embed/videoseries?list=" + res + "&autoplay=1&cc_load_policy=0&controls=0&iv_load_policy=3&modestbranding=1&showinfo=0&autohide=1";
     
     this.PlaylistURL += this.loop ? "&loop=1" : "&loop=0";
-    this.PlaylistURL += "&index=" + this.index;
+    
+    //shuffle
+    // this.PlaylistURL += "&index=" + this.index;
   }
   
 }
